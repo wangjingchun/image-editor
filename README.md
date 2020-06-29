655e0af59d1b399e290afe3120233847cf9b10b3
wangjingchun <975235798@qq.com>
2020-06-29 11:26:42 +0800

    20200629

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-### 图片处理类
+### Image Editor Library
+
+##### resizeExact
+
+```php
+use wangjingchun\ImageEditor;
+
+$newImage = ImageEditor::resizeExact($basepath, $image, $width, $height);
+```
+
+
+##### resizeExactHeight
+
+```php
+use wangjingchun\ImageEditor;
+
+$newImage = ImageEditor::resizeExactHeight($basepath, $image, $height);
+```
+
+
+
+##### resizeExactWidth
+
+```php
+use wangjingchun\ImageEditor;
+
+$newImage = ImageEditor::resizeExactWidth($basepath, $image, $width);
+```
+
+
+##### resizeFill
+
+```php
+use wangjingchun\ImageEditor;
+
+$newImage = ImageEditor::resizeFill($basepath, $image, $width, $height);
+```
+
+
+
+##### resizeFit
+
+```php
+use wangjingchun\ImageEditor;
+
+$newImage = ImageEditor::resizeFit($basepath, $image, $width, $height);
+```
--- src/ImageEditor.php
+++ src/ImageEditor.php
@@ -10,9 +10,11 @@ use Grafika\Grafika;
 class ImageEditor
 {
     /*
-     * resizeFill
+     * resizeExact
+     *
+     * Resize image to exact dimensions ignoring aspect ratio
      */
-    public static function resizeFill($basepath, $image)
+    public static function resizeExact($basepath, $image, $width, $height)
     {
         $dirname = pathinfo($image, PATHINFO_DIRNAME);
         $filename = pathinfo($image, PATHINFO_BASENAME);
@@ -20,12 +22,101 @@ class ImageEditor
         $editor = Grafika::createEditor();
 
         $imageObj = null;
-        $imageResizeFill = $dirname . '/thumb_' . $filename;
+        $imageResize = $dirname . '/thumb_' . $filename;
 
         $editor->open($imageObj, $basepath . $image);
-        $editor->resizeFill($imageObj, 200, 200);
-        $editor->save($imageObj, $basepath . $imageResizeFill);
+        $editor->resizeExact($imageObj, $width, $height);
+        $editor->save($imageObj, $basepath . $imageResize);
 
-        return $imageResizeFill;
+        return $imageResize;
     }
+
+    /*
+     * resizeExactHeight
+     *
+     * Resize image to exact height. Width is auto calculated
+     */
+    public static function resizeExactHeight($basepath, $image, $height)
+    {
+        $dirname = pathinfo($image, PATHINFO_DIRNAME);
+        $filename = pathinfo($image, PATHINFO_BASENAME);
+
+        $editor = Grafika::createEditor();
+
+        $imageObj = null;
+        $imageResize = $dirname . '/thumb_' . $filename;
+
+        $editor->open($imageObj, $basepath . $image);
+        $editor->resizeExactHeight($imageObj, $height);
+        $editor->save($imageObj, $basepath . $imageResize);
+
+        return $imageResize;
+    }
+
+    /*
+     * resizeExactWidth
+     *
+     * Resize image to exact width. Height is auto calculated
+     */
+    public static function resizeExactWidth($basepath, $image, $width)
+    {
+        $dirname = pathinfo($image, PATHINFO_DIRNAME);
+        $filename = pathinfo($image, PATHINFO_BASENAME);
+
+        $editor = Grafika::createEditor();
+
+        $imageObj = null;
+        $imageResize = $dirname . '/thumb_' . $filename;
+
+        $editor->open($imageObj, $basepath . $image);
+        $editor->resizeExactWidth($imageObj, $width);
+        $editor->save($imageObj, $basepath . $imageResize);
+
+        return $imageResize;
+    }
+
+    /*
+     * resizeFill
+     *
+     * Resize image to fill all the space in the given dimension. Excess parts are cropped
+     */
+    public static function resizeFill($basepath, $image, $width, $height)
+    {
+        $dirname = pathinfo($image, PATHINFO_DIRNAME);
+        $filename = pathinfo($image, PATHINFO_BASENAME);
+
+        $editor = Grafika::createEditor();
+
+        $imageObj = null;
+        $imageResize = $dirname . '/thumb_' . $filename;
+
+        $editor->open($imageObj, $basepath . $image);
+        $editor->resizeFill($imageObj, $width, $height);
+        $editor->save($imageObj, $basepath . $imageResize);
+
+        return $imageResize;
+    }
+
+    /*
+     * resizeFit
+     *
+     * Resize an image to fit within the given width and height. The re-sized image will not exceed the given dimension
+     */
+    public static function resizeFit($basepath, $image, $width, $height)
+    {
+        $dirname = pathinfo($image, PATHINFO_DIRNAME);
+        $filename = pathinfo($image, PATHINFO_BASENAME);
+
+        $editor = Grafika::createEditor();
+
+        $imageObj = null;
+        $imageResize = $dirname . '/thumb_' . $filename;
+
+        $editor->open($imageObj, $basepath . $image);
+        $editor->resizeFit($imageObj, $width, $height);
+        $editor->save($imageObj, $basepath . $imageResize);
+
+        return $imageResize;
+    }
+
 }
